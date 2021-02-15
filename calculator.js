2bc04ed7a87235da69884d85fa5671f024505681
Eddie <github.towage@aleeas.com>
2021-02-14 17:49:12 -0800

    Added BMI Calculator

--- calculator.js
+++ calculator.js
@@ -11,6 +11,20 @@ app.get("/", function(req, res){
   res.sendFile(__dirname + "/index.html")
 });
 
+app.get("/bmicalculator", function (req, res){
+  res.sendFile(__dirname + "/bmiCalculator.html")
+})
+
+app.post("/bmiCalculator", function(req, res){
+var height = parseFloat(req.body.height);
+var weight = parseFloat(req.body.weight);
+
+var result = weight / (height * height);
+
+res.send("Its " + result)
+
+})
+
 app.post("/", function(req, res){
   var num1 = Number(req.body.num1);
   var num2 = Number(req.body.num2);
